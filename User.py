5877287b6ba486b705432b14414d44b792b97537
difoster7 <95183391+difoster7@users.noreply.github.com>
2022-01-06 14:25:25 -0500

    planning updated with more user and character details
    User Class created

---
+++ User.py
@@ -0,0 +1,39 @@
+# User Class
+from datetime import date
+
+class User:
+
+    def __init__(self, name):
+        self.name = name
+        self.join_date = date.today()
+        self.games_played = 0
+        self.character_slots = 1
+        self.characters = []
+        self.active_character = None
+
+    def create_new_character(self, char_name):
+        """
+        Creates a new character if there's available space
+        :param name: (str) name of the character
+        :return: 0 if success, -1 general error, -2 no character slots left
+        """
+        if len(self.characters) >= self.character_slots:
+            return -1
+        else:
+            try:
+                self.characters.append(None) # Fix when characters exist
+            except BaseException:
+                # Add something in here that prints to an error log
+                return -2
+            return 0
+
+    def switch_active_character(self, char_name):
+        """
+        Switches the active character
+        :param char_name: (str) name of the character to switch to
+        :return: 0 on success, -1 general error, -2 if character is not found
+        """
+
+        # search for char_name in characters[]
+        # update active_char if found
+        return -1
